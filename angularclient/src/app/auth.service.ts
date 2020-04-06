35876930b8c5a2dba4161c23513fe5a8b3f0a34a
Alfonso Tienda <atienda@iprocuratio.com>
2020-04-06 19:34:23 +0200

    logiin component v1

---
+++ angularclient/src/app/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AuthService {
+
+  uri = 'http://localhost:8080';
+  token: string;
+
+  constructor(private http: HttpClient, private router: Router) { };
+
+
+  login(user: string, password: string) {
+    this.http.post(this.uri + '/login/', { user: user, password: password })
+      .subscribe(
+        (resp: any) => {
+          console.log("respuesta", resp);
+          this.router.navigate(['users']);
+          localStorage.setItem('auth_token', resp.token);
+        })
+  }
+
+  // borra el token del almacenamiento local
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  // devuelve un boleano que determina si un usuario está autenticado
+  public get logIn(): boolean {
+    return (localStorage.getItem('token') !== null);
+  }
+
+}
+
+
+
+
+
